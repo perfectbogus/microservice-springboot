6d1e0336f21200fc496692fe213f9f684ab360fc
eduardo escobar <war_army_10@hotmail.com>
2021-02-19 12:09:12 -0600

    Validating Test Cases, Adding Valitations for our DTO

--- src/main/java/dev/perfectbogus/multiplication/challenge/ChallengeAttemptDTO.java
+++ src/main/java/dev/perfectbogus/multiplication/challenge/ChallengeAttemptDTO.java
@@ -2,10 +2,23 @@ package dev.perfectbogus.multiplication.challenge;
 
 import lombok.Value;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 @Value
 public class ChallengeAttemptDTO {
+
+    @Min(1) @Max(99)
     int factorA;
+
+    @Min(1) @Max(99)
     int factorB;
+
+    @NotBlank
     String userAlias;
+
+    @Positive
     int guess;
 }
